ff07224e9dfd4621ac332ad9d303f95fd6780a95
Davi <daviqrozz2@gmail.com>
2025-11-24 15:11:21 -0300

    feat:edit view finished

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import product_view,create_view
+from .views import product_view,create_view,edit_view
 
 urlpatterns = [
     path('', product_view, name='products_view'),
-    path('/create', create_view, name='create_view')
+    path('create', create_view, name='create_view'),
+    path('edit/<int:id>/', edit_view, name='edit_view')
 ]
--- api/views.py
+++ api/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render
-# Importe a constante STATUS_OPTIONS do seu models.py
-from .models import Product, Category, STATUS_OPTIONS 
-
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Product,STATUS_OPTIONS
+from .forms import ProductForm
 
 def product_view(request):
     
-   
     products = Product.objects.all()
  
-    
-
     selected_status = request.GET.get('status')
     
     status_filter_value = None
     
-
     if selected_status and selected_status.isdigit():
  
         status_filter_value = int(selected_status)
@@ -22,13 +17,11 @@ def product_view(request):
     if status_filter_value is not None:
 
         products = products.filter(status=status_filter_value)
-            
 
     context = {
         'products_list': products,
         'selected_status': selected_status, 
         'status_choices': STATUS_OPTIONS,    
-    
     }
     
     return render(request, 'views/products.html', context)
@@ -36,3 +29,24 @@ def product_view(request):
 
 def create_view(request):
     return render(request, 'views/create.html')
+
+def edit_view(request, id):
+    product = get_object_or_404(Product, pk=id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        
+        if form.is_valid():
+
+            form.save()
+            return redirect('products_view')
+
+    else:
+        form = ProductForm(instance=product)
+        
+    context = {
+        'form': form,
+        'product': product
+    }
+    
+    return render(request, 'views/edit.html', context)
